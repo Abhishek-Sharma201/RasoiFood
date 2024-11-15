cc1b484bf553648c7752349de36b58456c4228d4
Abhishek Sharma <abhishek.webdev001@gmail.com>
2024-11-15 12:08:57 +0530

    Nav Footer --Skeleton

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React from "react";
 
 const Nav = () => {
@@ -7,28 +8,48 @@ const Nav = () => {
     window.addEventListener("resize", () =>
       setSmallScreen(window.innerWidth <= 768 ? true : false)
     );
-  }, []);
+  }, [smallScreen]);
 
   return (
-    <nav className="w-full h-[10dvh] flex items-center justify-between px-28 bg-zinc-950">
+    <nav className="text-white w-full h-[10dvh] lg:px-28 flex items-center justify-between px-12 bg-zinc-950">
       <div className="w-[max-content] h-full flex items-center justify-center cursor-pointer">
         Logo
       </div>
       {smallScreen ? (
-        ""
+        <span>Menu</span>
       ) : (
         <ul className="w-[max-content] h-full p-3 gap-8 flex items-center justify-between">
           <li className="w-[max-content] h-full p-2 flex items-center justify-center cursor-pointer">
-            Home
+            <Link
+              href={"/"}
+              className="text-[.9rem] hover:border-b-1 hover:border-b-blue-500 hover:border-b-2"
+            >
+              Home
+            </Link>
           </li>
           <li className="w-[max-content] h-full p-2 flex items-center justify-center cursor-pointer">
-            About
+            <Link
+              href={"/about"}
+              className="text-[.9rem] hover:border-b-1 hover:border-b-blue-500 hover:border-b-2"
+            >
+              About
+            </Link>
           </li>
           <li className="w-[max-content] h-full p-2 flex items-center justify-center cursor-pointer">
-            Login
+            <Link
+              href={"/login"}
+              className="text-[.9rem] hover:border-b-1 hover:border-b-blue-500 hover:border-b-2"
+            >
+              Login
+            </Link>
           </li>
           <li className="w-[max-content] h-full p-2 flex items-center justify-center cursor-pointer">
-            Dashboard
+            <Link
+              href={"/dashboard"}
+              className="text-[.9rem] hover:border-b-1 hover:border-b-blue-500 hover:border-b-2"
+            >
+              Dashboard
+            </Link>
           </li>
         </ul>
       )}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 
 const page = () => {
   return (
     <div>
       <Nav />
-      {/* <Footer /> */}
+      <Hero />
+      <Footer />
     </div>
   );
 };
